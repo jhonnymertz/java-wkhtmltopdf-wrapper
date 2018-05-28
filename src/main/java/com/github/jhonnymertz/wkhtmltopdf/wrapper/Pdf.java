48ed9490d86305c3fc6b9b44839d064fd723c933
kdenzel <kai.denzel@uni-ulm.de>
2018-05-28 18:56:57 +0200

    -Updated test classes, so i can compile successfully (even on a windows machine without Xvfb) -Added attribute filePath to the page Class -Temp-Files now gets deleted after creating from html string

--- src/main/java/com/github/jhonnymertz/wkhtmltopdf/wrapper/Pdf.java
+++ src/main/java/com/github/jhonnymertz/wkhtmltopdf/wrapper/Pdf.java
@@ -27,27 +27,27 @@ import java.util.concurrent.TimeUnit;
  * Represents a Pdf file
  */
 public class Pdf {
-
+    
     private static final Logger logger = LoggerFactory.getLogger(Pdf.class);
-
+    
     private static final String STDINOUT = "-";
-
+    
     private final WrapperConfig wrapperConfig;
-
+    
     private final Params params;
-
+    
     private final Params tocParams;
-
+    
     private final List<Page> pages;
-
+    
     private boolean hasToc = false;
-
+    
     private int timeout = 10;
-
+    
     public Pdf() {
         this(new WrapperConfig());
     }
-
+    
     public Pdf(WrapperConfig wrapperConfig) {
         this.wrapperConfig = wrapperConfig;
         this.params = new Params();
@@ -86,43 +86,43 @@ public class Pdf {
     public void addPageFromFile(String source) {
         this.pages.add(new Page(source, PageType.file));
     }
-
+    
     public void addToc() {
         this.hasToc = true;
     }
-
+    
     public void addParam(Param param, Param... params) {
         this.params.add(param, params);
     }
-
+    
     public void addTocParam(Param param, Param... params) {
         this.tocParams.add(param, params);
     }
-
+    
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
-
+    
     public File saveAs(String path) throws IOException, InterruptedException {
         File file = new File(path);
         FileUtils.writeByteArrayToFile(file, getPDF());
         logger.info("PDF successfully saved in {}", file.getAbsolutePath());
         return file;
     }
-
+    
     public byte[] getPDF() throws IOException, InterruptedException, PDFExportException {
-
+        
         ExecutorService executor = Executors.newFixedThreadPool(2);
-
+        
         try {
             logger.debug("Generating pdf with: {}", getCommand());
             Process process = Runtime.getRuntime().exec(getCommandAsArray());
-
+            
             Future<byte[]> inputStreamToByteArray = executor.submit(streamToByteArrayTask(process.getInputStream()));
             Future<byte[]> outputStreamToByteArray = executor.submit(streamToByteArrayTask(process.getErrorStream()));
-
+            
             process.waitFor();
-
+            
             if (process.exitValue() != 0) {
                 byte[] errorStream = getFuture(outputStreamToByteArray);
                 logger.error("Error while generating pdf: {}", new String(errorStream));
@@ -130,7 +130,7 @@ public class Pdf {
             } else {
                 logger.debug("Wkhtmltopdf output:\n{}", new String(getFuture(outputStreamToByteArray)));
             }
-
+            
             logger.info("PDF successfully generated with: {}", getCommand());
             return getFuture(inputStreamToByteArray);
         } finally {
@@ -139,38 +139,40 @@ public class Pdf {
             cleanTempFiles();
         }
     }
-
+    
     private String[] getCommandAsArray() throws IOException {
         List<String> commandLine = new ArrayList<String>();
-
+        
         if (wrapperConfig.isXvfbEnabled()) {
             commandLine.addAll(wrapperConfig.getXvfbConfig().getCommandLine());
         }
-
+        
         commandLine.add(wrapperConfig.getWkhtmltopdfCommand());
-
+        
         commandLine.addAll(params.getParamsAsStringList());
-
+        
         if (hasToc) {
             commandLine.add("toc");
             commandLine.addAll(tocParams.getParamsAsStringList());
         }
-
+        
         for (Page page : pages) {
             if (page.getType().equals(PageType.htmlAsString)) {
-
+                
                 File temp = File.createTempFile("java-wkhtmltopdf-wrapper" + UUID.randomUUID().toString(), ".html");
                 FileUtils.writeStringToFile(temp, page.getSource(), "UTF-8");
-
+                page.setFilePath(temp.getAbsolutePath());
                 commandLine.add(temp.getAbsolutePath());
             } else {
+                //Add source
                 commandLine.add(page.getSource());
             }
         }
         commandLine.add(STDINOUT);
+        logger.debug(commandLine.toString());
         return commandLine.toArray(new String[commandLine.size()]);
     }
-
+    
     private Callable<byte[]> streamToByteArrayTask(final InputStream input) {
         return new Callable<byte[]>() {
             public byte[] call() throws Exception {
@@ -178,7 +180,7 @@ public class Pdf {
             }
         };
     }
-
+    
     private byte[] getFuture(Future<byte[]> future) {
         try {
             return future.get(this.timeout, TimeUnit.SECONDS);
@@ -186,18 +188,19 @@ public class Pdf {
             throw new RuntimeException(e);
         }
     }
-
+    
     private void cleanTempFiles() {
         logger.debug("Cleaning up temporary files...");
         for (Page page : pages) {
             if (page.getType().equals(PageType.htmlAsString)) {
-                new File(page.getSource()).delete();
+                logger.debug("Delete temp file at: " + page.getFilePath());
+                new File(page.getFilePath()).delete();
             }
         }
     }
-
+    
     public String getCommand() throws IOException {
         return StringUtils.join(getCommandAsArray(), " ");
     }
-
+    
 }
