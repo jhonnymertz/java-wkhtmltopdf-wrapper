11f5d87bcb2dca70edbf9107b63fe3d31784547c
Jhonny Mertz <jhonnymertz@gmail.com>
2019-02-03 19:41:25 -0200

    setting up travis to deploy only on master

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false"  ]; then
-	echo "PR"
-  echo "$TRAVIS_PULL_REQUEST"
-    #mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    #exit $?
-fi
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "master"
-  echo "$TRAVIS_BRANCH"
+if [[ ($TRAVIS_PULL_REQUEST == "false") && ($TRAVIS_BRANCH == "master") ]]; then
+    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    exit $?
 fi
