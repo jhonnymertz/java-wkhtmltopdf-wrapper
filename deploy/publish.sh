7521722f574b0bb39046c3e3987d8fcba4724767
Jhonny Mertz <jhonnymertz@gmail.com>
2019-02-03 19:37:28 -0200

    testing travis if

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
+if [ "$TRAVIS_PULL_REQUEST" == "false"  ]; then
+	echo "PR"
+  echo "$TRAVIS_PULL_REQUEST"
+    #mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    #exit $?
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "master"
+  echo "$TRAVIS_BRANCH"
 fi
