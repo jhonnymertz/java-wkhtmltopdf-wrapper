9de5af716380918ba032fe97f1a10a18034bd6e4
jhonnymertz <jhonnymertz@gmail.com>
2019-02-07 17:24:42 -0200

    testing travis-ci variables

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
+    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "This will deploy!"
+else
+  echo "This will not deploy!"
+fi
+
+echo "$TRAVIS_BRANCH"
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_TAG"
+
 if [[ ($TRAVIS_PULL_REQUEST == "false") && ($TRAVIS_BRANCH == "master") ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    #openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+    #openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+    #mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
