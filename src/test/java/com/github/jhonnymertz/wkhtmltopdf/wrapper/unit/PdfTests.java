e06a119a51ccb6dc5a90b7d9e77f6e8e800d3b0f
Worik McShane <wmcshane@geometryit.com>
2023-10-09 22:51:45 +1100

    Added unit and integration tests to cover new command possibilities. Scenarios covered: - using wkhtmltopdf object types toc, page and cover - object ordering - providing per object params - wrapper config 'alwaysPutTocFirst'

--- src/test/java/com/github/jhonnymertz/wkhtmltopdf/wrapper/unit/PdfTests.java
+++ src/test/java/com/github/jhonnymertz/wkhtmltopdf/wrapper/unit/PdfTests.java
@@ -4,6 +4,9 @@ import com.github.jhonnymertz.wkhtmltopdf.wrapper.Pdf;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.configurations.FilenameFilterConfig;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.configurations.WrapperConfig;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.configurations.XvfbConfig;
+import com.github.jhonnymertz.wkhtmltopdf.wrapper.objects.Cover;
+import com.github.jhonnymertz.wkhtmltopdf.wrapper.objects.Page;
+import com.github.jhonnymertz.wkhtmltopdf.wrapper.objects.TableOfContents;
 import com.github.jhonnymertz.wkhtmltopdf.wrapper.params.Param;
 import org.junit.After;
 import org.junit.Assert;
@@ -96,9 +99,63 @@ public class PdfTests {
     }
 
     @Test
+    public void testTocParamsUsingTocObject() throws IOException {
+        TableOfContents toc = pdf.addToc();
+        toc.addParam(new Param("--test-param"), new Param("--test-param2", "test-value"));
+        pdf.addPageFromUrl("http://www.google.com");
+        Assert.assertThat("command params should contain toc params", pdf.getCommand(), containsString("--test-param2 test-value"));
+    }
+
+    @Test
     public void testXvfbCommand() throws Exception {
         wc.setXvfbConfig(new XvfbConfig());
         pdf = new Pdf(wc);
         Assert.assertThat("command should contain xvfb-run config", pdf.getCommand(), containsString("xvfb-run"));
+    }
+
+    @Test
+    public void testTocAlwaysFirstByDefault() throws Exception {
+        pdf.addPageFromUrl("http://www.google.com");
+        pdf.addToc();
+        pdf.addPageFromFile("test.html");
+        Assert.assertThat("command params should contain toc before pages", pdf.getCommand(), containsString("wkhtmltopdf toc http://www.google.com test.html -"));
+    }
+
+    @Test
+    public void testTocCustomLocation() throws Exception {
+        wc.setAlwaysPutTocFirst(false);
+        pdf.addPageFromUrl("http://www.google.com");
+        pdf.addToc();
+        pdf.addPageFromFile("test.html");
+        Assert.assertThat("command params should contain toc after url page and before file page", pdf.getCommand(), containsString("wkhtmltopdf http://www.google.com toc test.html -"));
+    }
+
+    @Test
+    public void testPageParams() throws IOException {
+        Page page1 = pdf.addPageFromUrl("http://www.google.com");
+        page1.addParam(new Param("--exclude-from-outline"));
+        Page page2 = pdf.addPageFromFile("test.html");
+        page2.addParam( new Param("--zoom", "2"));
+        Assert.assertThat("command url page should contain page specific params", pdf.getCommand(), containsString("http://www.google.com --exclude-from-outline"));
+        Assert.assertThat("command file page should contain page specific params", pdf.getCommand(), containsString("test.html --zoom"));
+    }
+
+    @Test
+    public void testCoverParams() throws IOException {
+        Cover cover = pdf.addCoverFromFile("cover.html");
+        cover.addParam(new Param("--test-param"), new Param("--test-param2", "test-value"));
+        pdf.addPageFromUrl("http://www.google.com");
+        Assert.assertThat("command params should contain cover params", pdf.getCommand(), containsString("cover cover.html --test-param --test-param2 test-value"));
+    }
+
+    @Test
+    public void testMulipleObjects() throws IOException {
+        wc.setAlwaysPutTocFirst(false);
+        pdf.addCoverFromFile("cover.html");
+        pdf.addPageFromFile("foreword.html");
+        pdf.addToc();
+        pdf.addPageFromUrl("http://www.google.com");
+        pdf.addPageFromFile("test.html");
+        Assert.assertThat("command should match the order objects are added", pdf.getCommand(), containsString("wkhtmltopdf cover cover.html foreword.html toc http://www.google.com test.html -"));
     }
 }
